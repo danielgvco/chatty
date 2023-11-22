01321f03a9628222539fa2c3ff85d93fc8d597c2
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-11-21 20:32:20 -0800

    fixed an error

--- lib/utils.ts
+++ lib/utils.ts
@@ -31,7 +31,7 @@ export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
   const yAxisLabels = [];
-  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
+  const highestRecord = Math.max(...revenue.map((month) => month.profit));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
   for (let i = topLabel; i >= 0; i -= 1000) {
